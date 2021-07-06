7efdbeecd3edd98ecb387a015795b7d2f9740c9d
folkforms <alan.bergin@gmail.com>
2021-07-07 00:39:29 +0100

    Read .npm-release.yml and execute commands

--- npm-release.js
+++ npm-release.js
@@ -1,1 +1,15 @@
-console.log("### npm-release.js");
+#!/usr/bin/env node
+
+const fileio = require("@folkforms/file-io");
+const shelljs = require("shelljs");
+
+// FIXME if(argument === "genconfig") { generate default config }
+
+const contents = fileio.readLines(".npm-release.yml");
+contents.forEach(line => {
+  const result = shelljs.exec(line);
+  if(result.code != 0) {
+    console.log(`Error running command '${line}'`);
+    shelljs.exit(code);
+  }
+});
